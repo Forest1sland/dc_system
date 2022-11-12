5e92601e678b1707d9c5b19aed1a09c612342a80
Forest1sland <1548893870@qq.com>
2022-11-12 23:20:34 +0800

    update

---
+++ src/main/java/com/java/dc_system/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.java.dc_system.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @Author: zjt
+ * @Date: 2022-11-11 23:36
+ * @Description:
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
+    }
+}
